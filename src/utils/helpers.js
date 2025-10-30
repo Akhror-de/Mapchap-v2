5e2ca428fae3f0b245dfbfbfe8f6ad25789f88f4
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-10-30 17:53:30 +0300

    Create helpers.js

---
+++ src/utils/helpers.js
@@ -0,0 +1,19 @@
+export const debounce = (func, wait) => {
+  let timeout
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout)
+      func(...args)
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+  }
+}
+
+export const generateId = () => {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2)
+}
+
+export const formatDate = (date) => {
+  return new Date(date).toLocaleDateString('ru-RU')
+}
