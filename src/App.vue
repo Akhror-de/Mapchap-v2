57acd689dca21e2a05e08aabd4678d1306b5e817
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-10-30 11:43:21 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <h1>MapChap App</h1>
+    <p>Структура создается...</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.app {
+  text-align: center;
+  padding: 20px;
+}
+</style>
