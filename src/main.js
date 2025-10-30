cdc81fa54bc580d179a4e28d4fbc26ba93d6b5d1
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-10-31 00:14:35 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import './styles/variables.css'
 import './styles/base.css'
 import './styles/components.css'
+import './styles/map.css' // ✅ Добавляем стили для карты
 
 const app = createApp(App)
 const pinia = createPinia()
