4535aed6d7cfe7dadf72694b2c5147e563be0a7a
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-10-31 00:13:31 +0300

    Update map.store.js

--- src/stores/map.store.js
+++ src/stores/map.store.js
@@ -1,8 +1,55 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useMapStore = defineStore('map', {
-  state: () => ({
-    map: null,
-    userLocation: null
-  })
+export const useMapStore = defineStore('map', () => {
+  const map = ref(null)
+  const userLocation = ref(null)
+  const currentZoom = ref(12)
+  const currentCenter = ref([55.7558, 37.6173])
+  const isMapReady = ref(false)
+
+  const setMap = (mapInstance) => {
+    map.value = mapInstance
+    isMapReady.value = true
+    
+    // Следим за изменением зума и центра
+    mapInstance.events.add('boundschange', () => {
+      currentZoom.value = mapInstance.getZoom()
+      currentCenter.value = mapInstance.getCenter()
+    })
+  }
+
+  const setUserLocation = (location) => {
+    userLocation.value = location
+  }
+
+  const setCenter = (coordinates, zoom = null) => {
+    if (map.value) {
+      if (zoom) {
+        map.value.setCenter(coordinates, zoom)
+      } else {
+        map.value.setCenter(coordinates)
+      }
+      currentCenter.value = coordinates
+    }
+  }
+
+  const setZoom = (zoom) => {
+    if (map.value) {
+      map.value.setZoom(zoom)
+      currentZoom.value = zoom
+    }
+  }
+
+  return {
+    map,
+    userLocation,
+    currentZoom,
+    currentCenter,
+    isMapReady,
+    setMap,
+    setUserLocation,
+    setCenter,
+    setZoom
+  }
 })
