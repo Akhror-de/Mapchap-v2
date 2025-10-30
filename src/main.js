58779ffb79533c9ba03fe0a68b92c4fdac7c10c3
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-10-30 17:38:02 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Импортируем стили
+import './styles/variables.css'
+import './styles/base.css'
+import './styles/components.css'
+
 const app = createApp(App)
 const pinia = createPinia()
 
