f39870ebfe28cdffb4014513972773c8a06ea92d
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-10-30 17:54:11 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <AppHeader />
     <MapContainer />
     <BottomSheet />
+    
+    <BusinessPanel v-if="appStore.activePanel === 'business'" />
+    <ProfilePanel v-if="appStore.activePanel === 'profile'" />
   </div>
 </template>
 
@@ -11,10 +14,8 @@ import { useAppStore } from './stores/app.store'
 import AppHeader from './components/common/AppHeader.vue'
 import MapContainer from './components/map/MapContainer.vue'
 import BottomSheet from './components/common/BottomSheet.vue'
+import BusinessPanel from './components/business/BusinessPanel.vue'
+import ProfilePanel from './components/profile/ProfilePanel.vue'
 
 const appStore = useAppStore()
 </script>
-
-<style>
-/* Базовые стили уже подключены в main.js */
-</style>
