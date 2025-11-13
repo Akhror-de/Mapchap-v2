94b6b6b6ea8c661a98176d3e2cd355f10eab4d47
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-11-13 20:05:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,15 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000
-  }
+  },
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  base: './'
 })
