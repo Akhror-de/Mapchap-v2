410d5f4b44387141a188cd7804f531617c98ed2f
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-11-15 08:56:49 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-// Импортируем Яндекс Карты
-import YMapPlugin from 'vue-yandex-maps'
+// Импортируем Яндекс Карты правильно
+import * as VueYandexMaps from 'vue-yandex-maps'
 
 const app = createApp(App)
 app.use(createPinia())
 
-// Настройки Яндекс Карт с вашим ключом
+// Настройки Яндекс Карт
 const yandexMapsSettings = {
   apiKey: '07b74146-5f5a-46bf-a2b1-cf6d052a41bb',
   lang: 'ru_RU',
@@ -16,5 +16,5 @@ const yandexMapsSettings = {
   version: '2.1'
 }
 
-app.use(YMapPlugin, yandexMapsSettings)
+app.use(VueYandexMaps.default, yandexMapsSettings)
 app.mount('#app')
